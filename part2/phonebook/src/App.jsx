cf2373e93fb1d5ea5527a54d85d16afa75a8d133
alexwisco <alexw_arv91z7@DESKTOP-P6PVHOP.(none)>
2024-11-27 16:16:58 -0500

    Done 2.6 - comments added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,21 +7,23 @@ const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('')
 
-  //
+  // When user types in form submission box
   const handleNameChange = (e) => {
     setNewName(e.target.value)
   }//
 
-  //
+  // User has clicked the submit button (the event e).
+  // Creates new person object with the name the user has 
+  // typed in. Adds new person to phonebook, also printing it. 
   const addPerson = (e) => {
-    console.log('beep')
+    //console.log('beep')
     e.preventDefault()
 
-    //
     const personObj = {
       name: newName
-    } //
+    } 
 
+    // Adding new person to phonebook
     setPersons(persons.concat(personObj))
     console.log('new person added. Name: ', personObj.name)
 
